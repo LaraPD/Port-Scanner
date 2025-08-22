3ed892b75343c19c3298a54a0d83ad2eccdee4ca
Lara Pérez Durán <121726522+LaraPD@users.noreply.github.com>
2025-08-22 02:25:57 +0200

    Add port_scanner.py
    Educational project

--- port_scanner.py
+++ port_scanner.py
@@ -1,23 +1,40 @@
-# PORT SCANNER 
+# PORT SCANNER
 
 # The objective of this script is to create a simple port scanner that checks for open ports on a target host.
 
 # Import the socket library that allows us to create network connections from Python
 import socket
 
-# Ask the user for the target host to scan
+# Ask the user for the target host and the range of ports to scan
 ip = input("Enter the IP address to scan: ")
+start_port = int(input("Enter the starting port: "))
+end_port = int(input("Enter the ending port: "))
+
+open_ports = []
+
+print(f"\n🔎 Scanning host {ip}\n" + "..." + "\n")
+print("OPEN ports ✅ | CLOSED ports ❌\n")
+print("-" * 32)
 
 # Scans a range of ports and verifies their status: open or closed
-for port in range(0, 65535):
+for port in range(start_port, end_port + 1):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Specifies the use of IPv4 (AF_INET) and TCP (SOCK_STREAM)
-    sock.settimeout(5) 
-
+    sock.settimeout(0.5)
+    
     result = sock.connect_ex((ip, port))
-
+    
     if result == 0:
-        print(f"Port {port} is OPEN ✅")
-        sock.close()
+        print(f"Port {port:<5} ✅")
+        open_ports.append(port)
     else:
-        print(f"Port {port} is CLOSED ❌")
-        sock.close()
+        print(f"Port {port:<5} ❌")
+    
+    sock.close()
+
+print("-" * 32)
+
+if open_ports:
+    print(f"\n🔵 SUMMARY: Open ports {open_ports}")
+else:
+    print(f"\n🔴 SUMMARY: No open ports found.\n")
+print("\n")
